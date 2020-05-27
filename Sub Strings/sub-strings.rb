2cd6064f4570254afd9051d5b27020a7da0bf294
dyrits <dyrits@outlook.fr>
2020-05-27 15:00:41 +0200

    Minor update.

--- Sub Strings/sub-strings.rb
+++ Sub Strings/sub-strings.rb
@@ -1,8 +1,6 @@
 def substrings(substring, dictionnary)
   substring.downcase!
   words_count = Hash.new(0)
-  dictionnary.each do |word|
-    words_count[word] += substring.scan(word).count if substring.include? word
-  end
+  dictionnary.each { |word| words_count[word] += substring.scan(word).count if substring.include? word }
   words_count
 end
