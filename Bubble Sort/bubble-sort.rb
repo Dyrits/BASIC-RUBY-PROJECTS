2fe8b3274afdccbe37bfe71cf53853d77c9e401f
dyrits <dyrits@outlook.fr>
2020-05-29 15:34:20 +0200

    Minor update.

--- Bubble Sort/bubble-sort.rb
+++ Bubble Sort/bubble-sort.rb
@@ -1,6 +1,6 @@
 def bubble_sort(array)
   iteration = array.length
-  while iteration > 0
+  while iteration > 1 do
     array.slice(0, iteration - 1).map!.with_index {|value, index|
     array[index], array[index + 1] = array[index + 1], array[index] if array[index] > array[index + 1]
     }
